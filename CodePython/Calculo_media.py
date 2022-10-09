ca012f16a566449d4eee13d4738c333ff849de12
Davi Gomes <davigomes.m1@gmail.com>
2022-10-09 16:54:09 -0300

    Create codes

---
+++ CodePython/Calculo_media.py
@@ -0,0 +1,9 @@
+nota1 = int(input("digite a primeira nota do aluno: "))
+nota2 = int(input("digite a segunda nota do aluno: "))
+media = (nota1 + nota2) / 2
+print("A media do aluno é: ", media)
+
+#OU:
+#nota1 = int(input("digite a primeira nota do aluno: "))
+#nota2 = int(input("digite a segunda nota do aluno: "))
+#print("A media do aluno é: {}".format((nota1 + nota2) / 2))
---
+++ CodePython/Transformando_metros_cm_mm.py
@@ -0,0 +1,2 @@
+n = int(input("digite um valor em metros: "))
+print("esse valor em cm é {} e em milimetros é {}".format(n*100, n*1000))
