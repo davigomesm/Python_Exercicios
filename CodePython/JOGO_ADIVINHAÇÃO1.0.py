897a87b6c38d2cb195d4bbf3af08dc93baa3f9dd
Davi Gomes <davigomes.m1@gmail.com>
2022-10-12 10:14:19 -0300

    adicionando sleep no jogo de adivinhação

--- CodePython/JOGO_ADIVINHAÇÃO1.0.py
+++ CodePython/JOGO_ADIVINHAÇÃO1.0.py
@@ -1,9 +1,12 @@
 from random import randint
+from time import sleep
 computador = randint(0, 5)
 print("COMPUTADOR: -VOU SORTEAR UM NUMERO DE 0 A 5, TENTE ACERTAR.")
 print("=-"*40)
 jogador = int(input("Digite um número de 0 a 5: "))
 print("=-"*40)
+print("\033[34mCALCULANDO...\033[m")
+sleep(2)
 print("O NUMÉRO SORTEADO FOI O {}".format(computador))
 if jogador == computador:
     print("\033[32mPARABENS VOCE ACERTOU O NÚMERO\033[m")
