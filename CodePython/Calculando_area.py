cd32054951b065c0106e18365aedda0a9cc2158e
Davi Gomes <davigomes.m1@gmail.com>
2022-10-12 08:08:14 -0300

    Create Calculo area

---
+++ CodePython/Calculando_area.py
@@ -0,0 +1,4 @@
+l = int(input("digite a largura da parede em metros: "))
+a = int(input("digite a altura da parede em metros: "))
+area = l * a
+print("a area da parede é de {} metros quadrados".format(area))
