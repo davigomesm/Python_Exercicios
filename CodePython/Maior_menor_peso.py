9a2aa6beb55cf275185c8814ebf823152e3305e7
Davi Gomes <davigomes.m1@gmail.com>
2022-11-27 20:26:38 -0300

    Concertando logica de maoir menor

--- CodePython/Maior_menor_peso.py
+++ CodePython/Maior_menor_peso.py
@@ -1,10 +1,13 @@
 pesomaior = 0
 pesomenor = 0
-for pessoa in range(1, 6):
+for pessoa in range(0, 5):
     peso = float(input("Digite o peso da {}ª pessoa: kg".format(pessoa)))
-    if pesomaior < peso:
-        pesomaior = peso
-    elif pesomaior > peso:
-        pesomenor = peso
+    if pessoa == 0:
+        pesomaior = pesomenor = peso
+    else:
+        if peso > pesomaior:
+            pesomaior = peso
+        if peso < pesomenor:
+            pesomenor = peso
 print("O maior peso é: {:.2f}kg".format(pesomaior))
 print("O menor peso é: {:.2f}kg".format(pesomenor))
