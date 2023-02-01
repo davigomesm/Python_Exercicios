debd1317c08137423284c84647f1341766a460a0
Davi Gomes <davigomes.m1@gmail.com>
2023-02-01 09:33:44 -0300

    Transferindo código para o arquivo

---
+++ Exercicios/003 - Somando dois números.py
@@ -0,0 +1,4 @@
+n1 = input("digite um numero: ")
+n2 = input("digite outro numero: ")
+soma = n1 + n2
+print("A soma de {} com {} é: {}".format(n1, n2, soma))
---
+++ Exercicios/005 - Antecessor e sucessor.py
@@ -0,0 +1,2 @@
+n1  = int(input("digite um numnero inteiro: "))
+print("o antessesor desse numero é {} e o sussesor é {}".format(n1-1, n1+1))
