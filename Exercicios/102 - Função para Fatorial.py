01a791d01defd47b230efd87da7235219dacc3f4
Davi Gomes <davigomes.m1@gmail.com>
2023-02-06 23:05:58 -0300

    Transferindo codigo para o arquivo

--- Exercicios/102 - Função para Fatorial.py
+++ Exercicios/102 - Função para Fatorial.py
@@ -1,18 +1,16 @@
 def fatorial(num, show=False):
-    fat = 1
-    for i in range(num, 0, -1):
-        fat *= i
-    if show:
-        for i in range(num, 0, -1):
-            if i == 1:
-                print('1', end='')
+    f = 1
+    for c in range(num, 0, -1):
+        if show:
+            print(c, end="")
+            if c > 1:
+                print(" x ", end="")
             else:
-                print(i, end=' x ')
-        print(f' = {fat}')
-    else:
-        print(f'Fatorial de {num}! é {fat}')
+                print(" = ", end="")
+        f *= c
+    return f
 
 
 n = int(input('Informe um número para calcular o fatorial: '))
-mostrar = int(input('Mostrar na tela? [0-Não/1-Sim]'))
-fatorial(n, mostrar)
+mostrar = int(input("Digite 1 para mostrar na tela ou 0 para não mostrar: "))
+print(fatorial(n, mostrar))
