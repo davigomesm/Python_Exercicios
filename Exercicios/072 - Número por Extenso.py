7b34922d49ca9fa0ed201f77296a7df1d1266397
Davi Gomes <davigomes.m1@gmail.com>
2023-02-08 10:48:46 -0300

    transferindo codigo para o arquivo

--- Exercicios/072 - Número por Extenso.py
+++ Exercicios/072 - Número por Extenso.py
@@ -1,3 +1,7 @@
+#   Exercício Python 072 - Número por Extenso
+#   Crie um programa que tenha uma tupla totalmente preenchida com uma contagem por extenso, de zero até vinte.
+#   Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
+
 tupla = ("zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove", "dez", "onze", "doze", "treze", "quatorze", "quinze", "dezesseis", "dezessete", "dezoio", "dezenove", "vinte")
 valor = int(input("digite um valor de 0 a 20: "))
 while valor > 20 or valor < 0:
